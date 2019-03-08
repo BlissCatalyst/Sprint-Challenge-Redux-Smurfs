b29ea2b05c8835ebc371c6315c2a2efbd4898987
Logan Hufstetler <logan.hufstetler@gmail.com>
2019-03-08 09:55:24 -0700

    built out actions with api requests. Also biolerplated reducer.

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,7 +1,13 @@
+import axios from 'axios';
+
 /* 
   Action Types Go Here!
   Be sure to export each action type so you can pull it into your reducer
 */
+export const ADDSMURF = "ADDSMURF";
+export const GETSMURF = "GETSMURF";
+export const UPDATESMURF = "UPDATESMURF";
+export const DELETESMURF = "DELETESMURF";
 
 /*
   For this project you'll need at least 2 action creators for the main portion,
@@ -13,3 +19,22 @@
    U - updateSmurf
    D - deleteSmurf
 */
+export const addSmurf = newSmurf => dispatch => {
+  dispatch({ type: ADDSMURF });
+  axios
+  .post('http://localhost:3333/smurfs', newSmurf)
+  .then(res => console.log(res))
+  .catch(err => {
+    dispatch({ type: "NEW_SMURF_ERROR", payload: err });
+  });
+};
+
+export const getSmurf = () => dispatch => {
+  dispatch({ type: GETSMURF });
+  axios
+  .get('http://localhost:3333/smurfs')
+  .then(res => console.log(res))
+  .catch(err => {
+    dispatch({ type: "GET_SMURF_ERROR" });
+  });
+};
--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,6 +1,12 @@
 /*
   Be sure to import in all of the action types from `../actions`
 */
+import {
+  ADDSMURF,
+  GETSMURF,
+  UPDATESMURF,
+  DELETESMURF
+} from '../actions';
 
 /*
  Your initial/default state for this project could *Although does not have to* look a lot like this
@@ -13,6 +19,14 @@
    error: null
  }
 */
+const initialState = {
+  smurfs: [],
+   fetchingSmurfs: false,
+   addingSmurf: false,
+   updatingSmurf: false,
+   deletingSmurf: false,
+   error: null
+}
 
 /*
   You'll only need one smurf reducer for this project.
@@ -21,3 +35,23 @@
   There is no need for 'combineReducers' in this project.
   Components can then read your store as, `state` and not `state.fooReducer`.
 */
+export default function reducer(state = initialState, action) {
+  switch(action.type) {
+    case ADDSMURF:
+      return {
+        ...state
+      };
+    case GETSMURF:
+      return {
+        ...state
+      };
+    case UPDATESMURF:
+      return {
+        ...state
+      };
+    case DELETESMURF:
+      return {
+        ...state
+      };
+  }
+}
